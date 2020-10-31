da68d242c041f75be0af3b164af11414bdcf54bc
Charles P <charles.prystupa@gmail.com>
2020-10-31 01:13:52 +0000

    Function w. Recursion: sum from 2 given values

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let hiVal = 0;
+  let lowVal = 0;
+
+  if (fromN > toN) {
+    hiVal = fromN;
+    lowVal = toN;
+  } else {
+    hiVal = toN;
+    lowVal = fromN;
+  }
+  // Counts Down-----------------
+  if (hiVal === lowVal) { // <<< BASE CASE
+    return lowVal;
+  }
+
+  return hiVal + sum((hiVal - 1), lowVal); // << RECURSION CASE
 }
 
 module.exports = sum;
